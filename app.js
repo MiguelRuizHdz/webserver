419569bf2fba8c9dc90dac8304d8b6d48f57c811
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2021-07-21 21:40:59 -0500

    Despliegue app Angular - 2021-07-21

--- app.js
+++ app.js
@@ -13,34 +13,39 @@ hbs.registerPartials( __dirname + '/views/partials');
 // Servir contenido estático
 app.use( express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('home', {
-      nombre: 'Miguel Ruiz',
-      titulo: 'Curso de Node'
-  });
-});
+// app.get('/', (req, res) => {
+//   res.render('home', {
+//       nombre: 'Miguel Ruiz',
+//       titulo: 'Curso de Node'
+//   });
+// });
+
 // app.get('/hola-mundo', (req, res) => {
 //   res.send('Hola Mundo')
 // });
 
-app.get('/generic', (req, res) => {
-    res.render('generic', {
-        nombre: 'Miguel Ruiz',
-        titulo: 'Curso de Node'
-    });
-});
+// app.get('/generic', (req, res) => {
+//     res.render('generic', {
+//         nombre: 'Miguel Ruiz',
+//         titulo: 'Curso de Node'
+//     });
+// });
 
-app.get('/elements', (req, res) => {
-    res.render('elements', {
-        nombre: 'Miguel Ruiz',
-        titulo: 'Curso de Node'
-    });
+// app.get('/elements', (req, res) => {
+//     res.render('elements', {
+//         nombre: 'Miguel Ruiz',
+//         titulo: 'Curso de Node'
+//     });
+// });
+
+app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/index.html')
 });
  
-app.get('*', (req, res) => {
-//   res.send('404 | Page not found')
-    res.sendFile( __dirname + '/public/404.html')
-});
+// app.get('*', (req, res) => {
+// //   res.send('404 | Page not found')
+//     res.sendFile( __dirname + '/public/404.html')
+// });
  
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
